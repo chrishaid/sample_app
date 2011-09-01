eeea9dd4a4cc5f0e320e1b857a6a089d046df044
chrishaid <chrishaid@gmail.com>
2011-09-01 12:20:18 -0500

    Done with user edit/update, index, and destory actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
   def new
     @title = "Sign in"
   end
-
+  
   def create
     user = User.authenticate(params[:session][:email],
                              params[:session][:password])
@@ -13,7 +13,7 @@ class SessionsController < ApplicationController
       render 'new'
     else
       sign_in user
-      redirect_to user
+      redirect_back_or user
     end
   end
   
@@ -21,4 +21,5 @@ class SessionsController < ApplicationController
     sign_out
     redirect_to root_path
   end
+  
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -13,6 +13,7 @@ module SessionsHelper
     @current_user ||= user_from_remember_token
   end
   
+  
   def signed_in?
     !current_user.nil?
   end
@@ -20,6 +21,20 @@ module SessionsHelper
   def sign_out
     cookies.delete(:remember_token)
     self.current_user = nil
+  end
+  
+  def current_user?(user)
+    user==current_user
+  end
+  
+  def deny_access
+    store_location
+    redirect_to signin_path, :notice => "Please sign in to access this page."
+  end
+  
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    clear_return_to
   end
   
   private
@@ -31,5 +46,11 @@ module SessionsHelper
       cookies.signed[:remember_token] || [nil, nil]
     end
     
+    def store_location
+      session[:return_to] = request.fullpath
+    end
     
+    def clear_return_to
+      session[:return_to] = nil
+    end
 end
